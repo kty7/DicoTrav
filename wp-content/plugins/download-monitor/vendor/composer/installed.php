22029a2471128fdc407aeeed31ee43277438cabf
Samuel <kejinfreche@gmail.com>
2025-01-23 21:01:31 +0100

    starting from the bottom now we here

---
+++ wp-content/plugins/download-monitor/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpchill/download-monitor',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '49ebbb45cf9a130f9c28ae54bb08a373538a6c4f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpchill/download-monitor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '49ebbb45cf9a130f9c28ae54bb08a373538a6c4f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
