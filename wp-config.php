f751dd2d2e49272b94306a7f82eb94cfbf8a7632
kty7 <romaindorkety@gmail.com>
2025-01-27 07:45:41 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'DicoTrav' );
+define( 'DB_NAME', 'dicoTrav' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
